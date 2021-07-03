cada3d7b50d6d50009d8b8f70c4b170e466dfac6
prasanna <cs20btech11029@iith.ac.in>
2021-07-03 09:22:01 +0530

    Added contents in menustate folder

---
+++ menustate/include/DEFINITIONS.hpp
@@ -0,0 +1,17 @@
+# pragma once
+
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 605
+
+#define SPLASH_STATE_SHOW_TIME 0.8
+#define SPLASH_STATE_FILEPATH "resources/Images/BackGrounds/splashscreenbackground.PNG"
+#define MAIN_MENU_STATE_FILEPATH "resources/Images/BackGrounds/menu state background.jpg"
+
+
+
+#define MAIN_MENU_FONTS_FILEPATH "resources/fonts/Marker Felt.ttf"
+
+
+#define PLAY_BUTTON_FILEPATH "resources/Images/buttons/playbutton.png"
+#define HELP_BUTTON_FILEPATH "resources/Images/buttons/helpbutton.png"
+#define EXIT_BUTTON_FILEPATH "resources/Images/buttons/exitbutton.png"
