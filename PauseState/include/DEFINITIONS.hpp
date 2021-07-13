3a49b74fde8f20cf8de0d4218d42c53b9931ea3b
spdanda <saipravallikadanda@gmail.com>
2021-07-13 15:07:51 +0530

    Added Gamestate and Pause State

---
+++ PauseState/include/DEFINITIONS.hpp
@@ -0,0 +1,38 @@
+# pragma once
+
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 720
+
+#define SPLASH_STATE_SHOW_TIME 3
+
+#define SPLASH_STATE_FILEPATH "resources/Images/BackGrounds/splashscreenbackground.PNG"
+#define MAIN_MENU_STATE_FILEPATH "resources/Images/BackGrounds/menu state background.jpg"
+#define HELP_STATE_FILEPATH "resources/Images/BackGrounds/help state background.jpg"
+#define GAME_WINDOW_STATE_FILEPATH "resources/Images/BackGrounds/Gameplay.jpg"
+#define PAUSE_BACKGROUND_FILEPATH "resources/Images/BackGrounds/Pausebackground.jpg"
+
+#define MARKER_FONT_FILEPATH "resources/fonts/Marker Felt.ttf"
+#define PACIFICO_FONT_FILEPATH "resources/fonts/Pacifico-Regular.ttf"
+#define ARIAL_FONT_FILEPATH "resources/fonts/arial.ttf"
+
+#define PLAY_BUTTON_FILEPATH "resources/Images/buttons/playbutton.png"
+#define HELP_BUTTON_FILEPATH "resources/Images/buttons/helpbutton.png"
+#define EXIT_BUTTON_FILEPATH "resources/Images/buttons/exitbutton.png"
+#define PAUSE_BUTTON_FILEPATH "resources/Images/buttons/pausebutton.png"
+#define RESUME_BUTTON_FILEPATH "resources/Images/buttons/resumebutton.png"
+#define RESTART_BUTTON_FILEPATH "resources/Images/buttons/restartbutton.png"
+
+#define GRID_SPRITE_FILEPATH "resources/Images/BackGrounds/Board.png"
+
+#define BLUE_BUBBLE_FILEPATH "resources/Images/Bubbles/blue.png"
+#define GREEN_BUBBLE_FILEPATH "resources/Images/Bubbles/GREEN.png"
+#define PINK_BUBBLE_FILEPATH "resources/Images/Bubbles/pink.png"
+#define RED_BUBBLE_FILEPATH "resources/Images/Bubbles/red.png"
+#define SILVER_BUBBLE_FILEPATH "resources/Images/Bubbles/silver.png"
+#define YELLOW_BUBBLE_FILEPATH "resources/Images/Bubbles/yellow.png"
+
+#define STATE_PLAYING 98
+#define STATE_PAUSED 97
+#define STATE_WON 96
+#define STATE_LOSE 95
+#define STATE_MATCHING_BUBBLE 94
---
+++ PauseState/src/GameState.cpp
@@ -0,0 +1,94 @@
+#include "GameState.hpp"
+#include <iostream>
+#include <sstream>
+
+
+namespace BubbleBash
+{
+    GameState::GameState(GameDataRef data) : _data(data)
+    {
+    }
+
+    void GameState::Init()
+    {
+        gameState = STATE_PLAYING;
+
+        this->_data->assets.LoadTexture("Background", GAME_WINDOW_STATE_FILEPATH);
+        this->_data->assets.LoadTexture("Grid Sprite", GRID_SPRITE_FILEPATH);
+
+        this->_data->assets.LoadTexture("Pause Button", PAUSE_BUTTON_FILEPATH);
+        this->_data->assets.LoadTexture("Help Button", HELP_BUTTON_FILEPATH);
+        
+        _background.setTexture(this->_data->assets.GetTexture("Background"));
+        _gridSprite.setTexture(this->_data->assets.GetTexture("Grid Sprite"));
+
+        _pausebutton.setTexture(this->_data->assets.GetTexture("Pause Button"));
+        _helpbutton.setTexture(this->_data->assets.GetTexture("Help Button"));
+       
+
+        _gridSprite.setPosition(500, 200);
+
+        _pausebutton.setPosition(50,400);
+        _helpbutton.setPosition(50,500);
+
+        if (!font.loadFromFile(MARKER_FONT_FILEPATH))
+        {
+            throw("COULD NOT LOAD FONT");
+        }
+
+        text_time.setFont(font);
+        text_time.setCharacterSize(35);
+        text_time.setColor(sf::Color::Yellow);
+        text_time.setPosition(50, 100);
+
+        text_score.setFont(font);
+        text_score.setCharacterSize(35);
+        text_score.setColor(sf::Color::Yellow);
+        text_score.setPosition(50, 155);
+
+        text_time.setString("Time Remaining : ");
+        text_score.setString("Current Score : ");
+    }
+
+    void GameState::HandleInput()
+    {
+        sf::Event event;
+
+        while (this->_data->window.pollEvent(event))
+        {
+            if (sf::Event::Closed == event.type)
+            {
+                this->_data->window.close();
+            }
+
+            else if (this->_data->input.IsSpriteClicked(this->_pausebutton, sf::Mouse::Left, this->_data->window))
+            {
+                this->_data->machine.AddState(StateRef(new PauseState(_data)), false);
+            }
+            else if (this->_data->input.IsSpriteClicked(this->_helpbutton, sf::Mouse::Left, this->_data->window))
+            {
+                this->_data->machine.AddState(StateRef(new HelpState(_data)), true);
+            }
+        }
+    }
+
+    void GameState::Update(float dt)
+    {
+    }
+
+    void GameState::Draw(float dt)
+    {
+        this->_data->window.clear(sf::Color::Black);
+
+        this->_data->window.draw(this->_background);
+        this->_data->window.draw(this->_gridSprite);
+
+        this->_data->window.draw(this->_pausebutton);
+        this->_data->window.draw(this->_helpbutton);
+
+        this->_data->window.draw(text_time);
+        this->_data->window.draw(text_score);
+
+        this->_data->window.display();
+    }
+}
